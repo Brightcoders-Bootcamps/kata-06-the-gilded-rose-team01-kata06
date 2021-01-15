9381cd456e87622b7665100b30ca437c8c333a7f
Raul morales abarca <r20160106@gmail.com>
2021-01-15 03:17:08 -0600

    refactoring update_inventory
    some errors were corrected with robocop
    optimized the conditionals in the update_inventory file

--- src/update_inventory.rb
+++ src/update_inventory.rb
@@ -1,27 +1,20 @@
 require_relative 'item.rb'
 
-class UpdateInventory <Item
+# class update inventory
+class UpdateInventory < Item
   def stats
     sell_in_stats
     update_quality
   end
 
   def update_quality
-    if @sell_in >0
-      @quality -= 1
-    end
-
-    if @sell_in<0
-      @quality-=2
-    end
-
-    if @quality<0
-      @quality=0
-    end
-    @quality = 50 if @quality >= 50
+    if @sell_in > 0 then @quality -= 1 end
+    if @sell_in < 0 then @quality -= 2 end
+    if @quality < 0 then @quality = 0 end
+    @quality = 50 if @sell_in >= 50
   end
 
   def sell_in_stats
-    @sell_in-=1
+    @sell_in -= 1
   end
 end
